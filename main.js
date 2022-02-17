1da4a6b7596ee38f0b7d3be5f3ecfeb9c32ec28c
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-02-17 22:54:06 +0300

    Edit

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
-
 var canvas = document.getElementById("canvas"),
 originalImage = null,
 imageGrey = null,
 imageRed = null,
 imageRainbow = null;
 
-function loadImage() {
+function loadImage(){
 var file = document.getElementById("file");
 originalImage = new SimpleImage(file);
 imageGrey = new SimpleImage(file);
 imageRed = new SimpleImage(file);
 imageRainbow = new SimpleImage(file);
-
 originalImage.drawTo(canvas);
 }
-
-function filterGrey() {
+function filterGrey(){
 // Reset Image
-for(var pixel of imageGrey.values()) {
+for(var pixel of imageGrey.values()){
     var originalPixel = originalImage.getPixel(pixel.getX(), pixel.getY());
     imageGrey.setPixel(pixel.getX(), pixel.getY(), originalPixel)
 }
-
 // Grey Filter
-for(var pixel of imageGrey.values()) {
+for(var pixel of imageGrey.values()){
     var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
     pixel.setRed(avg);
     pixel.setGreen(avg);
@@ -31,25 +27,22 @@ for(var pixel of imageGrey.values()) {
 }
 imageGrey.drawTo(canvas);
 }
-
-function printOriginal(img) {
+function printOriginal(img){
 img.drawTo(canvas);
 }
-
-function makeRed() {
+function makeRed(){
 // Reset Image
-for(var pixel of imageRed.values()) {
+for(var pixel of imageRed.values()){
     var originalPixel = originalImage.getPixel(pixel.getX(), pixel.getY());
     imageRed.setPixel(pixel.getX(), pixel.getY(), originalPixel)
 }
-
 // Red Filter 	
-for (var pixel of imageRed.values()) {
+for (var pixel of imageRed.values()){
     var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
-    if(avg < 128) {
+    if(avg < 128){
         pixel.setRed(avg * 2 - 255);
         pixel.setBlue(avg * 2 - 255);
-    } else {
+    } else{
         pixel.setRed(255);
         pixel.setGreen(avg * 2 - 255);
         pixel.setBlue(avg * 2 - 255);
@@ -57,16 +50,14 @@ for (var pixel of imageRed.values()) {
 }
 imageRed.drawTo(canvas);
 }
-
-function makeRainbow() {
+function makeRainbow(){
 // Reset Image
-for(var pixel of imageRainbow.values()) {
+for(var pixel of imageRainbow.values()){
     var originalPixel = originalImage.getPixel(pixel.getX(), pixel.getY());
     imageRainbow.setPixel(pixel.getX(), pixel.getY(), originalPixel)
 }
-
 // Rainbow Filter
-for(var pixel of imageRainbow.values()) {
+for(var pixel of imageRainbow.values()){
     var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
     
     /* ---- Red Strip ----- */
@@ -81,7 +72,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(avg * 2 - 255);
         }
     }
-    
     /* ----- Orange Stripe ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (1/7) && pixel.getY() <= imageRainbow.getHeight() * (2/7)) {
         if(avg < 128) {
@@ -94,7 +84,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(avg * 2 - 255);
         }
     }
-    
     /* ----- Yellow Strip ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (2/7) && pixel.getY() <= imageRainbow.getHeight() * (3/7)) {
         if(avg < 128) {
@@ -107,7 +96,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(avg * 2 - 255);
         }
     }
-    
     /* ----- Green Strip ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (3/7) && pixel.getY() <= imageRainbow.getHeight() * (4/7)) {
         if(avg < 128) {
@@ -120,7 +108,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(avg * 2 - 255);
         }
     }
-    
     /* ----- Blue Strip ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (4/7) && pixel.getY() <= imageRainbow.getHeight() * (5/7)) {
         if(avg < 128) {
@@ -133,7 +120,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(255);
         }
     }
-    
     /* ----- Indigo Strip ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (5/7) && pixel.getY() <= imageRainbow.getHeight() * (6/7)) {
         if(avg < 128) {
@@ -146,7 +132,6 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(255);
         }
     }
-    
     /* ----- Violet Strip ----- */
     if(pixel.getY() > imageRainbow.getHeight() * (6/7) && pixel.getY() <= imageRainbow.getHeight() * (7/7)) {
         if(avg < 128) {
@@ -159,18 +144,15 @@ for(var pixel of imageRainbow.values()) {
             pixel.setBlue(0.4 * avg + 153);
         }
     }
-    
 }
 imageRainbow.drawTo(canvas);
 }
-
-function makeGrey() {
-if(imageGrey != null) {
+function makeGrey(){
+if(imageGrey != null){
     filterGrey();
 }
 }
-
-function resetImage() {
+function resetImage(){
 var context = canvas.getContext("2d");
 context.clearRect(0, 0, canvas.width, canvas.height);
 printOriginal(originalImage);
